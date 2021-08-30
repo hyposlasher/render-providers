413ab2044c8d7bd528d1aa73fc391654b1b10f24
Denis Maushov <dmaushov@gmail.com>
2021-08-30 11:39:22 +0300

    added provider with set state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import {useContext} from 'react'
-import { FooContext, BarContext, BazContext } from '.';
+import { FooContext, BarContext, BazContext, CounterContext } from '.';
 
 function App() {
   const foo = useContext(FooContext)
   const bar = useContext(BarContext)
   const baz = useContext(BazContext)
+  const [count, setCount] = useContext(CounterContext)
   
   return (
-    <p>
-      {foo} {bar} {baz}
-    </p>
+    <>
+
+      <p>
+        {foo} {bar} {baz} {count}
+      </p>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ export const FooContext = React.createContext();
 export const BarContext = React.createContext();
 export const BazContext = React.createContext();
 
+export const CounterContext = React.createContext();
+
+export function CounterContextProvider({ children }) {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <CounterContext.Provider value={[count, setCount]}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+
 const Providers = ({providers, children}) => {
   const renderProvider = (providers, children) => {
     const [provider, ...restProviders] = providers;
@@ -28,9 +40,11 @@ ReactDOM.render(
   <Providers providers={[
     <FooContext.Provider value="foo" />,
     <BarContext.Provider value="bar" />,
-    <BazContext.Provider baz="baz" />
+    <BazContext.Provider value="baz" />,
+    <CounterContextProvider />
   ]}>
     <App />
   </Providers>,
   document.getElementById('root')
 );
+
